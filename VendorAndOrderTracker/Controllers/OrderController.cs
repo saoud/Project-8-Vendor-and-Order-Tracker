d64615ad409a7abdbc8a1a020a2008780a5fc67d
Saoud <saoud@saoud.dev>
2021-05-15 16:44:35 -0700

    adding basic strcuture to OrderController with correct namespaces

---
+++ VendorAndOrderTracker/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
