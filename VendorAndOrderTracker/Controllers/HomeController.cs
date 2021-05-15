8f1e57db95c4d967bc78d92d5c58926d6b6b290d
Saoud <saoud@saoud.dev>
2021-05-15 16:43:19 -0700

    adding home controller with correct namespaces

---
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
