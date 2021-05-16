3c9229e5e8d1eb6080907ed1ecd95eed3002a8d3
Saoud <saoud@saoud.dev>
2021-05-15 20:34:19 -0700

    all vendor tests passing now

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -13,13 +13,29 @@ namespace VendorTracker.Tests
     {
       Vendor.ClearAll();
     }
-
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    public void VendorConstructor_CreateInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test", "test");
+      Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
-  }
-}
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
+    }
 
+  }
+
+}
