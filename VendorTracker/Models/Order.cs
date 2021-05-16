7d792b58a8d053ca8c110ddc8a3e9d38470c4e06
Saoud <saoud@saoud.dev>
2021-05-15 20:34:58 -0700

    adding order.cs and vendor.cs with correct code

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,10 +4,27 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    public string Title { get; set; }
     public string Description { get; set; }
     public int Id { get; }
-    public int Price { get; set; }
-    public string Date { get; set; } 
+    private static List<Order> _instances = new List<Order> { };
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+
   }
 }
