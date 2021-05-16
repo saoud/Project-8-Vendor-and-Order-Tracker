3741bbc79fac1a93891a925c32c4ebc274923d8d
Saoud <saoud@saoud.dev>
2021-05-15 17:00:00 -0700

    changing names of files to make it more readable

---
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using VendorTracker.Models;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
+
