1f2429458126528fb843f931249444d7daa71b0b
Saoud <saoud@saoud.dev>
2021-05-15 18:53:20 -0700

    adding in tests for ordertests.cs

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -5,19 +5,18 @@ using VendorTracker.Models;
 
 namespace VendorTracker.Tests
 {
+{
   [TestClass]
   public class OrderTests : IDisposable
   {
-
     public void Dispose()
     {
       Order.ClearAll();
     }
-
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread", "2 dozen Rye", 85, "5/1/21");
+      Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
   }
