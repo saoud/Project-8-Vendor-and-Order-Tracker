20523a48a54748fd41a8543ad48c26159d66fb7b
Saoud <saoud@saoud.dev>
2021-05-15 18:54:04 -0700

    adding in httppost paths  as well as basic function

--- VendorTracker/Controllers/OrderController.cs
+++ VendorTracker/Controllers/OrderController.cs
@@ -4,7 +4,29 @@ using VendorTracker.Models;
 
 namespace VendorTracker.Controllers
 {
-  public class OrdersController : Controller
+  public class OrderController : Controller
   {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }
